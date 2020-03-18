1ffde24a8003fa55aa4c77e4249b2013cae95535
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-03-18 17:46:42 +0100

    Added sub menu's

---
+++ dashboard/resources/views/partials/nav.blade.php
@@ -0,0 +1,44 @@
+<ul class="sidebar__menu nav my-3">
+    <li class="nav-item__section">
+        <h6 class="text-uppercase">Menu</h6>
+    </li>
+    <li class="nav-item">
+        <a class="nav__link collapsed" data-toggle="collapse" data-target="#dropdown1">Routes</a>
+        <div class="collapse" id="dropdown1" aria-expanded="false">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <span class="nav__link-text">Locations</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <span class="nav__link-text">Cities</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav__link collapsed" data-toggle="collapse" data-target="#dropdown2">Characters</a>
+        <div class="collapse" id="dropdown2" aria-expanded="false">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <span class="nav__link-text">Classes</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav__link">
+            <span class="nav__link-text">Users</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav__link">
+            <span class="nav__link-text">Other</span>
+        </a>
+    </li>
+</ul>
--- dashboard/resources/views/partials/sidebar.blade.php
+++ dashboard/resources/views/partials/sidebar.blade.php
@@ -3,30 +3,6 @@
         <h4>Admin Dashboard</h4>
     </div>
     <div class="sidebar__menu-wrapper">
-        <ul class="sidebar__menu nav my-3">
-            <li class="nav-item__section">
-                <h6 class="text-uppercase">Menu</h6>
-            </li>
-            <li class="nav-item">
-                <a class="nav__link">
-                    <span class="nav__link-text">Routes</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav__link">
-                    <span class="nav__link-text">Characters</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav__link">
-                    <span class="nav__link-text">Users</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav__link">
-                    <span class="nav__link-text">Other</span>
-                </a>
-            </li>
-        </ul>
+        @include("partials.nav")
     </div>
 </nav>
