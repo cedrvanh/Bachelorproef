d7111885ce11c28c2175a2effef2db3e65a022df
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-04-13 13:38:02 +0200

    Fixed android build

--- NativeApp/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-import AppNavigator from './routes';
-
-export default class App extends Component {
-    render() {
-        return <AppNavigator />
-    }
-}
--- NativeApp/src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import { Text } from 'react-native';
-
-const Button = () => {
-    return (
-        <Text>Button</Text>
-    )
-}
-
-export default Button;
