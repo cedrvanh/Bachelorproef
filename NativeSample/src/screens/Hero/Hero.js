8137d4da9930a3d3d0c9c829e2617042e60d8838
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-07-27 21:53:38 +0200

    Add model

---
+++ NativeSample/src/screens/Hero/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { colors, utils } from '~/styles';
+
+import Header from '~/components/Header';
+import Button from '~/components/Button';
+import HeroForm from '~/components/HeroForm';
+import InteractiveModel from '~/components/InteractiveModel';
+
+
+export default HeroScreen = ({ navigation }) => {
+    return (
+        <Container>
+            <Header title ={ 'Create a hero' } />
+            <HeroForm />
+            {/* <InteractiveModel /> */}
+            <Button label="Proceed" />
+        </Container>
+    )
+}
+
+const Container = styled.View`
+    flex: 1;
+    background-color: ${ colors.PRIMARY_COLOR };
+    padding: ${ utils.GUTTER_LARGE };
+`
