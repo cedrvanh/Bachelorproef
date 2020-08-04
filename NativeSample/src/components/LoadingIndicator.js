1049f456d0bbcf9c0c67425aa7d6c9e8e3392d89
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-08-04 19:26:34 +0200

    Added lottie loader

--- NativeSample/src/components/LoadingIndicator.js
+++ NativeSample/src/components/LoadingIndicator.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
+import Modal from 'react-native-modal';
 import styled from 'styled-components';
 
-import { colors, typography, utils } from '~/styles';
+import { colors } from '~/styles';
+
+import LottieWrapper from '~/components/LottieWrapper';
 
 export default LoadingIndicator = () => {
     return (
-        <Container>
-            <ActivityIndicator size="large" color={ colors.ACCENT_COLOR } />
-        </Container>
+        <Modal 
+            isVisible={true}
+            backdropColor={colors.PRIMARY_COLOR}
+            backdropOpacity={1}
+            style={{
+                flex: 1
+            }}
+        >
+            <Container>
+                <LottieWrapper 
+                    source={require('~/assets/lottie/loader.json')}
+                    style={{
+                        width: 250,
+                    }}
+                />
+            </Container>
+        </Modal>
     )
 }
 
 const Container = styled.View`
     flex: 1;
     justifyContent: center;
+    alignItems: center;
+    backgroundColor: ${ colors.PRIMARY_COLOR };
 `
