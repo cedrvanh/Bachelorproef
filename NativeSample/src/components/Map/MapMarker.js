7d333aaae953c4c279d23249633cc309454fd2aa
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-08-04 19:26:50 +0200

    Added custom callout to marker

--- NativeSample/src/components/Map/MapMarker.js
+++ NativeSample/src/components/Map/MapMarker.js
@@ -1,21 +1,65 @@
 import React from 'react';
-import { Marker } from 'react-native-maps';
+import { View, Text } from 'react-native';
+import { Marker, Callout } from 'react-native-maps';
+import styled from 'styled-components';
 
-import Icon from '../Icon';
+import { colors, typography, utils } from '~/styles';
+
+import Icon from '~/components/Icon';
 
 const MapMarker = ({ label, coords }) => {
     return (
         <Marker.Animated
             title={label}
             tracksViewChanges={false}
+            string={label}
             coordinate={{
                 latitude: coords.latitude, 
                 longitude: coords.longitude
             }}
         >
             <Icon name={"ios-pin"} size={42}/>
+
+            <Callout tooltip>
+                <View>
+                    <CalloutBubble>
+                        <Label>{label}</Label>
+                    </CalloutBubble>
+                    <CalloutArrow />
+                </View>
+            </Callout>
         </Marker.Animated>
     )
 }
 
-export default MapMakrker = React.memo(MapMarker);
+export default MemoMarker = React.memo(MapMarker);
+
+const CalloutBubble = styled.View`
+    width: 150px;
+    padding: 10px;
+    borderRadius: ${ utils.BORDER_RADIUS };
+    backgroundColor: ${ colors.WHITE };
+`
+
+const Label = styled.Text`
+    color: ${ colors.DARK_COLOR };
+    textAlign: center;
+    fontSize: 16px;
+    fontWeight: 900;
+`
+
+const CalloutArrow = styled.View`
+    width: 0px;
+    height: 0px;
+    marginTop: -3px;
+    alignSelf: center;
+    backgroundColor: transparent;
+    borderStyle: solid;
+    borderLeftWidth: 5px;
+    borderRightWidth: 5px;
+    borderBottomWidth: 10px;
+    borderLeftColor: transparent;
+    borderRightColor: transparent;
+    borderBottomColor: ${ colors.WHITE };
+    transform: rotate(180deg);
+`
