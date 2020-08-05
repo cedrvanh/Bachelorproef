7212c6aa9db14e315fc071ae8d5642abf16d3f8d
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-08-05 17:36:04 +0200

    Added Camera Screen with AR

--- NativeSample/src/screens/Camera/Camera.js
+++ NativeSample/src/screens/Camera/Camera.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text } from 'react-native';
 import styled from 'styled-components';
+import { ViroARSceneNavigator, ViroText, ViroConstants } from 'react-viro';
 
-export default ProfileScreen = () => {
+export default CameraScreen = () => {
+    var InitialARScene = require('~/js/HelloWorldSceneAR');
+    var UNSET = "UNSET";
+    var VR_NAVIGATOR_TYPE = "VR";
+    var AR_NAVIGATOR_TYPE = "AR";
+    
+    const [type, setType] = useState(UNSET);
+    const [state, setState] = useState({
+        text: 'Initializing AR...',
+    })
+
+    useEffect(() => {
+        setType(AR_NAVIGATOR_TYPE);
+
+        return () => setType(UNSET);
+    }, []);
+
+    _onInitialized = (state, reason) => {
+        console.log(state)
+
+        if (state == ViroConstants.TRACKING_NORMAL) {
+            console.log('Tracking')
+            setState({
+                text: 'Hello World'
+            });
+        } else if (state == ViroConstants.TRACKING_REASON_NONE) {
+            console.log('Loss of tracking')
+        }
+    }
+
+    // <Text>Camera</Text>
     return (
-        <Container>
-            <Title>Profile Screen</Title>
-        </Container>
+        <ViroARSceneNavigator initialScene={{scene: InitialARScene}} />
     )
 }
-
-const Container = styled.View`
-    background-color: green;
-    flex: 1;
-`;
-
-const Title = styled.Text`
-    color: blue;
-    fontSize: 24px;
-`;
